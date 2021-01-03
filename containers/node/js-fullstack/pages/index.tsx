e9aaee8d57d9c8085a9ccd0b2ab67a84fc5d3bfb
weakboson <weakboson@gmail.com>
2021-01-03 17:11:05 +0900

    セミコロンなしの方が好きだ

--- containers/node/js-fullstack/pages/index.tsx
+++ containers/node/js-fullstack/pages/index.tsx
@@ -1,6 +1,6 @@
 import { signIn, signOut, useSession } from "next-auth/client";
 export default function Home() {
-  const [session, loading] = useSession();
+  const [session, loading] = useSession()
   return (
     <>
       {!session && (
